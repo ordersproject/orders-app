6d6eb6e2df96f74ea7fcdcd48f2b8906902fd007
Vincent Tse <river_run@126.com>
2023-08-24 16:50:04 +0800

    fix: remove deleted page

--- src/routes.ts
+++ src/routes.ts
@@ -3,12 +3,10 @@ const Whitelist = () => import('./pages/Whitelist.vue')
 const Changelog = () => import('./pages/Changelog.vue')
 const Dev = () => import('./pages/Dev.vue')
 const Pool = () => import('./pages/Pool.vue')
-const Export = () => import('./pages/Export.vue')
 
 const routes = [
   { path: '/orders/:pair?', component: Home, alias: '/' },
   { path: '/whitelist', component: Whitelist },
-  { path: '/export', component: Export },
   { path: '/changelog', component: Changelog },
   { path: '/pool/:pair?', component: Pool },
   { path: '/dev', component: Dev },
