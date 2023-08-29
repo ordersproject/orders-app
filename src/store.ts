4b451b0c7522b1ed6aca086c264bd6fc24ec2d88
Vincent Tse <river_run@126.com>
2023-08-29 10:26:49 +0800

    fix

--- src/store.ts
+++ src/store.ts
@@ -13,8 +13,7 @@ export const useAddressStore = defineStore('address', {
   },
 
   getters: {
-    // get: (state) => state.address,
-    get: (state) => 'bc1qjmw7nrfaqkxxjz79u3wqdudzkjm2drp2ncqnqp',
+    get: (state) => state.address,
   },
 
   actions: {
